1660006f0c7373a41edf11e92fa84ba0f89b881f
EnPing Hsieh <shieh.npin@gmail.com>
2016-12-04 21:43:00 +0800

    max Sdk - > 21, Rename application

--- app/src/main/java/com/codepath/apps/mysimpletweet/HomeTimeline.java
+++ app/src/main/java/com/codepath/apps/mysimpletweet/HomeTimeline.java
@@ -2,13 +2,10 @@ package com.codepath.apps.mysimpletweet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
-import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -19,7 +16,7 @@ public class HomeTimeline extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_timeline);
 
-        RestApplication.getRestClient().getHomeTimeline(new JsonHttpResponseHandler(){
+        TweetApplication.getRestClient().getHomeTimeline(new JsonHttpResponseHandler(){
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
             }
